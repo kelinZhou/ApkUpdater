ec1e84d9b690cf5a871c8665ec111da0d7f21824
kelin <kelin.zhouql@lieluobo.com>
2018-12-12 13:36:51 +0800

    Update Demo。

--- app/src/main/java/com/kelin/updatemanagerdemo/MainActivity.java
+++ app/src/main/java/com/kelin/updatemanagerdemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.kelin.apkUpdater.ActivityStackManager;
 import com.kelin.apkUpdater.ApkUpdater;
 import com.kelin.apkUpdater.callback.IUpdateCallback;
 
@@ -40,19 +41,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private class ApkCompleteUpdateCallback implements IUpdateCallback {
+    private static class ApkCompleteUpdateCallback implements IUpdateCallback {
         private static final String TAG = "ApkCompleteUpdateCallback";
 
         @Override
         public void onSilentDownload(@NonNull ApkUpdater apkUpdater) {
-            Toast.makeText(getApplicationContext(), "静默下载", Toast.LENGTH_SHORT).show();
+            Toast.makeText(ActivityStackManager.getInstance().getApplicationContext(), "静默下载", Toast.LENGTH_SHORT).show();
             //apkUpdater.removeCallback();  //如果在用户点击静默安装之后不希望在监听后续的回调，则可以调用该方法。
         }
 
         @Override
         public void onSuccess(boolean isAutoCheck, boolean haveNewVersion, String curVersionName, boolean isForceUpdate) {
             if (!isAutoCheck && !haveNewVersion) {
-                Toast.makeText(getApplicationContext(), curVersionName + " 已是最新版本！", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ActivityStackManager.getInstance().getApplicationContext(), curVersionName + " 已是最新版本！", Toast.LENGTH_SHORT).show();
             }
         }
 
@@ -60,20 +61,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void onFiled(boolean isAutoCheck, boolean isCanceled, boolean haveNewVersion, String curVersionName, int checkMD5failedCount, boolean isForceUpdate) {
             if (isCanceled) {
                 if (!isAutoCheck) {
-                    Toast.makeText(getApplicationContext(), "更新被取消！" + curVersionName, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ActivityStackManager.getInstance().getApplicationContext(), "更新被取消！" + curVersionName, Toast.LENGTH_SHORT).show();
                 }
             } else {
                 if (isForceUpdate) {
-                    Toast.makeText(getApplicationContext(), "您必须升级后才能继续使用！" + curVersionName, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ActivityStackManager.getInstance().getApplicationContext(), "您必须升级后才能继续使用！" + curVersionName, Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(getApplicationContext(), "下载失败！" + curVersionName, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ActivityStackManager.getInstance().getApplicationContext(), "下载失败！" + curVersionName, Toast.LENGTH_SHORT).show();
                 }
             }
         }
 
         @Override
         public void onCompleted() {
-            Toast.makeText(getApplicationContext(), "完成", Toast.LENGTH_SHORT).show();
+            Toast.makeText(ActivityStackManager.getInstance().getApplicationContext(), "完成", Toast.LENGTH_SHORT).show();
         }
     }
 }
